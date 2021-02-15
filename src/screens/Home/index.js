7fabdaf06763bfa61b85bc99a47ff24a65e82185
khushal thepane <khushalthepane2000@gmail.com>
2021-02-15 22:18:25 +0530

    build

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -14,25 +14,43 @@ export default function Home() {
         baseURL: "https://europe-west1-boom-dev-7ad08.cloudfunctions.net/videoFeed",
     });
     
-    useEffect(() => {
-         
-            const fetchData = async () => {
-                const res = await Axios({
-                    method: 'post',
-                    url: '/',
-                    data: { "page": 0 }
-                  });
-        
-               
-                setVideos(res.data)
-            }
-            fetchData()
-            setDataProvider(dataProviderMaker(videos))
-
-      }, [])
     const dataProviderMaker = (data) => (new DataProvider((r1, r2) => r1 !== r2)).cloneWithRows(data)
     const [dataProvider, setDataProvider] = useState(dataProviderMaker(videos))
     
+    useEffect(() => {
+         
+            const fetchData = async () => {
+                try {
+                    const res = await Axios({
+                        method: 'post',
+                        url: '/',
+                        data: { "page": 0 }
+                      });
+                      
+                    setVideos(res.data)
+
+                } catch (error) {
+                console.log(error)   
+                }}
+                fetchData()
+                setDataProvider(dataProviderMaker(videos))
+                const fetchMoreData = async() => {
+                    try {
+                        const ress = await Axios({
+                            method: 'post',
+                            url: '/',
+                            data: { "page": 1 }
+                          });
+                    
+                        setVideos([...videos,ress.data])
+                    } catch (error) {
+                      console.log(error)  
+                    }
+                }
+            fetchMoreData()
+            setDataProvider(dataProviderMaker(videos))
+
+      }, [videos])
       const ViewTypes = {
           Full: 1
       }
@@ -51,14 +69,24 @@ export default function Home() {
         )
     }
 
-    return (<>
+    const loadNextPage = async () => {
+            const ress = await Axios({
+                method: 'post',
+                url: '/',
+                data: { "page": 1 }
+              });
         
+            setVideos([...videos,ress.data])
+    }
+
+    return (<>
             <View style={{minHeight: height, minWidth: width, flex: 1, flexDirection: 'column', }}>
             {videos.length ? ( <RecyclerListView 
                 style={{flex: 1, minHeight: height, minWidth: width,}}
                 layoutProvider={layoutProvider}
                 dataProvider={dataProvider}
                 rowRenderer={rowRenderer}
+                onEndReached={loadNextPage}
                 scrollViewProps={{
                     showsVerticalScrollIndicator: false,
                     snapToInterval: height,
